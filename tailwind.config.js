907fa09e9ece868ffe4e01ad5ee64fdd57dea197
Brett Siedschlag <36081344+BrettSiedschlag@users.noreply.github.com>
2023-09-23 09:32:32 -0400

    Final

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
+    screens: {
+      desktop: '1440px',
+    },
+    colors: {
+      orange: '#e38826',
+      cyan: '#006970',
+      darkCyan: '#004241',
+      white: 'hsla(0, 0%, 100%, 0.75)',
+      lightGray: 'hsl(0, 0%, 95%)',
+    },
+    fontFamily: {
+      'bigShoulders': ['"Big Shoulders Display"'],
+      'lexendDeca': ['"Lexend Deca"'],
+    },
+    extend: {
+      
+    },
   plugins: [],
-}
-
+}}
